b4c7fa190124ea360723e95079cfaa9f36d60ca0
9thdat <nthdat09@gmail.com>
2023-06-01 22:12:27 +0700

    Sua readme.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Fantastic-Four
+#Sửa giúp tui chỗ này theo đường dẫn máy mọi người:
+#MainPage.java dòng 85:
+#	File file = new File("D:\\DoAn\\Fantastic-Four\\src\\Asset\\Avatar\\DefaultAvatar.png"); // Sửa giúp chỗ này nha hix
--- src/View/MainPage/MainPage.java
+++ src/View/MainPage/MainPage.java
@@ -82,7 +82,7 @@ public class MainPage extends JFrame {
         }
         else {
             // Set default avatar
-            File file = new File("D:\\DoAn\\Fantastic-Four\\src\\Asset\\Avatar\\DefaultAvatar.png");
+            File file = new File("D:\\DoAn\\Fantastic-Four\\src\\Asset\\Avatar\\DefaultAvatar.png"); // Sửa giúp chỗ này nha hix
 
             // Convert file to byte[]
             FileInputStream fis = null;
